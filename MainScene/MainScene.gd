a1c4435836baf49e1cfc54a281fcb4bbd1caf9aa
Maplefin <james.huynh444@gmail.com>
2023-02-19 11:42:06 -0500

    fix popup issues

--- MainScene/MainScene.gd
+++ MainScene/MainScene.gd
@@ -1,10 +1,15 @@
 extends Node2D
 
 var videoclosed = true
-onready var tree = get_node("Files/Tree")
+onready var tree = get_node("GUI/Files/Tree")
+onready var viewer = get_node("GUI/Viewer")
+onready var off = get_node("GUI/Off")
+onready var noaccess = get_node("GUI/Access")
+onready var menu = get_node("GUI/PopupMenu")
+onready var files = get_node("GUI/Files")
 var tree_elements = []
 
-onready var handler = get_node("Viewer/ViewportContainer/Viewport/SceneHandler")
+onready var handler = get_node("GUI/Viewer/ViewportContainer/Viewport/SceneHandler")
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
@@ -24,47 +29,59 @@ func _physics_process(delta):
 		pm = true
 	var minute = timeDict.minute;
 	if !pm:
-		$Taskbar/Time.text = "%02d:%02d AM"%[hour,minute]
+		$GUI/Taskbar/Time.text = "%02d:%02d AM"%[hour,minute]
 	else:
-		$Taskbar/Time.text = "%02d:%02d PM"%[hour,minute]
+		$GUI/Taskbar/Time.text = "%02d:%02d PM"%[hour,minute]
 	
 	for i in range(len(tree_elements)):
 		if tree_elements[i] == tree.get_selected():
-			$Viewer.visible = true
-			putOnTop($Viewer)
+			putOnTop($GUI/Viewer)
+			$GUI/Viewer.visible = true
 			handler.scene = i
 
 
 func _on_StartButton_pressed():
-	get_node("PopupMenu").visible = !get_node("PopupMenu").visible
-	
+	if !menu.visible:
+		putOnTop(menu)
+		menu.visible = true
+	else:
+		menu.visible = false
 
 
 func _on_VideoButton_pressed():
-	$Viewer.visible = !$Viewer.visible
-	if $Viewer.visible:
-		putOnTop($Viewer)
+	if !viewer.visible:
+		putOnTop(viewer)
+		viewer.visible = true
+	else:
+		viewer.visible = false
+
 
 
 func _on_TextureButton_pressed():
-	$NoAccess.visible = true
-	putOnTop($NoAccess)
+	putOnTop(noaccess)
+	noaccess.visible = true
 
 
 func _on_ShutDown_pressed():
-	for i in get_children():
-		if i != $Off:
+	for i in $GUI.get_children():
+		if i != off:
 			i.visible = false
-	$Off.visible = true
-	putOnTop($Off)
+	off.visible = true
+	putOnTop(off)
 	get_tree().quit()
 
 
 func _on_Data_pressed():
-	$Files.visible = true
-	putOnTop($Files)
+	putOnTop(files)
+	files.visible = true
+	
 
 func putOnTop(control):
 	var parent = control.get_parent()
 	parent.move_child(control, parent.get_child_count())
 
+
+
+
+func _on_Viewer_gui_input(event):
+	print(event)
--- Scenes/SceneHandler.gd
+++ Scenes/SceneHandler.gd
@@ -20,3 +20,4 @@ func _input(event):
 	if Input.is_action_just_pressed("ui_left"):
 		if scene > 0:
 			scene -= 1
+
